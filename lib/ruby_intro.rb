34742088f94f4b5026fcadc6f0d438a80e82256d
EC2 Default User <ec2-user@ip-172-31-15-250.us-east-2.compute.internal>
2021-09-16 19:03:11 +0000

    Done with HW

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,68 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum(0)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return arr.max(2).sum(0)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return arr.combination(2).map {|v| v.sum()}.include? (n)
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s.downcase.start_with?(/[a-z]/) && s.downcase.start_with?(/[^aeiou]/)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.match?(/[^10]/)
+    return false
+  elsif s == ""
+    return false
+  else
+    dec = 0
+    s.split(//).reverse.map.with_index do |d, i|
+        dec += d.to_i*(2**i)
+    end
+    if dec%4 == 0
+        return true
+    else
+        return false
+    end
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price) 
+    if isbn != ""
+      @isbn = isbn
+    else
+      raise ArgumentError
+    end
+    
+    if price > 0
+      @price = price
+    else
+      raise ArgumentError
+    end
+  end
+  
+  def price_as_string
+    
+    return "$%.2f" %self.price.to_f
+    
+  end
+  
 end
